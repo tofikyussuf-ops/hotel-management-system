9bee3018e8a8e95599d87b1fa3d50920f011c96c
Tofik <tofikyussuf@gmail.com>
2026-02-23 15:25:44 +0300

    feat: setup sample data uploader and fix guestId mapping logic

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -3,7 +3,7 @@ import { useOutsideClick } from '../hooks/useOutsideClick';
 import Logo from './Logo';
 import MainNav from './MainNav';
 /* import Uploader from '../data/Uploader.jsx'; */
-import Uploader from '../data/Uploader.jsx';
+//import Uploader from '../data/Uploader.jsx';
 import Logout from '../features/authentication/Logout.jsx';
 
 function Sidebar() {
@@ -17,7 +17,7 @@ function Sidebar() {
     >
       <Logo />
       <MainNav />
-      <Uploader />
+      {/* <Uploader /> */}
 
       {/* Put the theme toggle and logout at the bottom */}
       <div className="mt-auto flex flex-col gap-4 border-t border-[var(--color-grey-100)] pt-8">
